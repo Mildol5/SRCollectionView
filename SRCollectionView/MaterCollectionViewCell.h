f4c07a8ea7b82a06a03878d060b10f60ea23672d
王颖兴 <wangyx@suirui.com>
2017-08-24 13:37:07 +0800

    master

---
+++ SRCollectionView/MaterCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MaterCollectionViewCell.h
+//  SRCollectionView
+//
+//  Created by 颖 on 2017/8/24.
+//  Copyright © 2017年 SUIRUI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MaterCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ SRCollectionView/MaterCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MaterCollectionViewCell.m
+//  SRCollectionView
+//
+//  Created by 颖 on 2017/8/24.
+//  Copyright © 2017年 SUIRUI. All rights reserved.
+//
+
+#import "MaterCollectionViewCell.h"
+
+@implementation MaterCollectionViewCell
+
+@end
